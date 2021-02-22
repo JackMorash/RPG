79cbb9e5c5b9d46458940b2017914acd44eed7c2
JackMorash <60446354+JackMorash@users.noreply.github.com>
2021-02-22 11:03:11 -0600

    Added functions to set weather type

--- or_events.py
+++ or_events.py
@@ -8,33 +8,38 @@ from or_globalvars import vars
 def cold_weather():
     """Random event for cold weather"""
 # Checks for sufficient clothing
-    enough_clothes = vars.amount_spent_on_clothing > 22 + \
-        (4 * random.random())
-    c_message = "" if enough_clothes else "Don't "
-    message = "[red]Cold weather, BRRRR! You {} have enough \
-clothing to keep you warm.[/red]".format(
-        c_message)
-    print(message)
-    if not enough_clothes:
-        vars.is_sufficient_clothing = True
-        vars.dead = True
+    while True:
+        vars.cold_weather == True
+        enough_clothes = vars.amount_spent_on_clothing > 22 + \
+            (4 * random.random())
+        c_message = "" if enough_clothes else "Don't "
+        message = "\n[red]Cold weather, BRRRR! You {} have enough \
+    clothing to keep you warm.[/red]\n".format(
+            c_message)
+        print(message)
+        if not enough_clothes:
+            vars.is_sufficient_clothing = True
+            vars.dead = True
 
 
 def heavy_rains():
     """Random event for heavy rain"""
-    print("Heavy rains...Time and supplies lost!")
-# Removes random amount of food, bullets, random supplies
-    vars.amount_spent_on_food -= 10
-    vars.amount_spent_on_bullets -= 500
-    vars.amount_spent_on_miscellaneous -= 15
-    vars.total_mileage -= (10 * random.random()) - 5
+    while True:
+        vars.raining = True
+        print("\nHeavy rains...Time and supplies lost!\n")
+    # Removes random amount of food, bullets, random supplies
+        vars.amount_spent_on_food -= 10
+        vars.amount_spent_on_bullets -= 500
+        vars.amount_spent_on_miscellaneous -= 15
+        vars.total_mileage -= (10 * random.random()) - 5
+        break
 
 
 def got_shot():
     """Random event for getting shot"""
-    print("[red]You've been shot in the leg!\
- they take one of your oxen.[/red]")
-    print("[blue]You ought to have a doctor look at your wound![/blue]")
+    print("\n[red]You've been shot in the leg!\
+ they take one of your oxen.[/red]\n")
+    print("\n[blue]You ought to have a doctor look at your wound![/blue]\n")
 # Applies injured var. Removes random amount of supplies, oxen
     vars.is_injured = True
     vars.amount_spent_on_miscellaneous -= 5
@@ -52,8 +57,8 @@ def weather():
 
 def wagon_break_down():
     """Random event for wagon breaking down"""
-    print("Your wagon breaks down! Time and supplies \
-have been lost during repairs.")
+    print("\n[red]Your wagon breaks down! Time and supplies \
+have been lost during repairs.[/red]\n")
 # Removes random amount of supplies, adds random miles
     vars.total_mileage -= 15 - (5 * random.random())
     vars.amount_spent_on_miscellaneous -= 8
@@ -61,8 +66,8 @@ have been lost during repairs.")
 
 def ox_injuries():
     """Random event for an ox injury"""
-    print("[red]An ox injures your leg! You are slowed \
-for the rest of the trip...[/red]")
+    print("\n[red]An ox injures your leg! You are slowed \
+for the rest of the trip...[/red]\n")
 # Increases amount spent on oxen, mileage
     vars.total_mileage -= 25
     vars.amount_spent_on_animals -= 20
@@ -70,8 +75,8 @@ for the rest of the trip...[/red]")
 
 def arm_broke():
     """Random event for a broken arm"""
-    print("[red]Uh oh! Your daughter broke her arm![/red]")
-    print("[blue]You stop and use some supplies to make a sling.[/blue]")
+    print("\n[red]Uh oh! Your daughter broke her arm![/red]\n")
+    print("\n[blue]You stop and use some supplies to make a sling.[/blue]\n")
 # Increases amount spent on supplies, mileage
     vars.total_mileage -= 5 - (4 * random.random())
     vars.amount_spent_on_miscellaneous -= 2 - (3 * random.random())
@@ -79,38 +84,38 @@ def arm_broke():
 
 def ox_wander():
     """Random event for an ox wandering off"""
-    print("[red]An ox wanders off, you take time to look for it.[/red]")
+    print("\n[red]An ox wanders off, you take time to look for it.[/red]\n")
 # Adds random amount to mileage
     vars.total_mileage -= 17
 
 
 def helpful_indians():
     """Random event for appearence of helpful indians"""
-    print("[/cyan]Helpful Native Americans help you look for food![/cyan]")
+    print("\n[cyan]Helpful Native Americans help you look for food![/cyan]\n")
 # Adds extra food to inventory
     vars.amount_spent_on_food += 14
 
 
 def lost_son():
     """Random event for your son getting lost"""
-    print("[red]Your son gets lost!\
- You spend the day looking for him...[/red]")
+    print("\n[red]Your son gets lost!\
+ You spend the day looking for him...[/red]\n")
 # Adds random amount to mileage
     vars.total_mileage -= 10
 
 
 def unsafe_water():
     """Random event for using unsafe water"""
-    print("[red]The water is contaminated, you take the time looking for a\
- fresh spring.[/red]")
+    print("\n[red]The water is contaminated, you take the time looking for a\
+ fresh spring.[/red]\n")
 # Adds random amount to mileage
     vars.total_mileage -= (10*random.random()) - 2
 
 
 def wagon_fire():
     """Random event for a wagon fire"""
-    print("[red]A fire has erupted in your wagon!\
- Some food and supplies were lost...[/red]")
+    print("\n[red]A fire has erupted in your wagon!\
+ Some food and supplies were lost...[/red]\n")
 # Removes random amount of food, bullets, supplies. Adds to mileage
     vars.amount_spent_on_food -= 40
     vars.amount_spent_on_bullets -= 400
@@ -120,29 +125,29 @@ def wagon_fire():
 
 def heavy_fog():
     """Random event for heavy fog"""
-    print("[red]A thick fog rolls in and you spend \
-time navigating through it[/red]")
+    print("\n[red]A thick fog rolls in and you spend \
+time navigating through it[/red]\n")
 # Adds random amount to mileage
     vars.total_mileage -= 10 - (5*random.random())
 
 
 def snake_poison():
     """Random event for getting bitten"""
-    print("[red]A poisonous snake bites you![/red]\
- [blue]Luckily you manage to kill it before it harms anybody else.[/blue]")
+    print("\n[red]A poisonous snake bites you![/red]\
+ [blue]Luckily you manage to kill it before it harms anybody else.[/blue]\n")
 # Removes bullets, supplies.
     vars.amount_spent_on_bullets -= 10
     vars.amount_spent_on_miscellaneous -= 5
 # Determines if player has enough supplies to live, if not, player dies.
     if vars.amount_spent_on_miscellaneous < 0:
-        print("[red]You die of a snakebite as you have no medicine.[/red]")
+        print("\n[red]You die of a snakebite as you have no medicine.[/red]\n")
         vars.dead = True
 
 
 def wagon_swamped():
     """Random event for getting your wagon swamped"""
-    print("[red]The wagon gets swamped while traversing a river, \
-some of your food and clothing gets lost.[/red]")
+    print("\n[red]The wagon gets swamped while traversing a river, \
+some of your food and clothing gets lost.[/red]\n")
 # Removes some food, clothing. Adds mileage
     vars.amount_spent_on_food -= 30
     vars.amount_spent_on_clothing -= 20
@@ -151,8 +156,8 @@ some of your food and clothing gets lost.[/red]")
 
 def hail_storm():
     """Random event for a hail storm"""
-    print("[red]A hail storm rolls in!\
- some of your supplies are damaged.[/red]")
+    print("\n[red]A hail storm rolls in!\
+ some of your supplies are damaged.[/red]\n")
 # Adds random amount to mileage, removes bullets and supplies
     vars.total_mileage -= 5 - (10*random.random())
     vars.amount_spent_on_bullets -= 200
@@ -163,31 +168,32 @@ def eating():
     """Determines if the food you eat will get you sick"""
     RND = random.random()
     if vars.choice_of_eating == 1:
-        vars.illness(vars)
+        vars.illness()
     elif vars.choice_of_eating == 3:
         if RND < .5:
-            vars.illness(vars)
+            vars.illness()
     else:
         if RND < .25:
-            vars.illness(vars)
+            vars.illness()
 
 
 def animals_attack():
     """Random event for a wolf attack"""
-    print("[red]Wild animals attack your group![/red]")
+    print("\n[red]Wild animals attack your group![/red]\n")
 # Determines shooting level, whether you miss or not, or have enough bullets
-    response_time = vars.shooting(vars.shooting_level)
+    response_time = vars.shooting()
     if vars.amount_spent_on_bullets <= 38:
-        print("[red]You were too low on bullets![/red]")
-        print("[red]The wolves overpower your group![/red]")
+        print("\n[red]You were too low on bullets![/red]\n")
+        print("\n[red]The wolves overpower your group![/red]\n")
         vars.is_injured = True
         vars.dead = True
 
     if response_time <= 2:
-        print("[cyan italic]Nice shootin'! They didn't get much.[cyan italic]")
+        print("\n[cyan italic]Nice shootin'! \
+They didn't get much.[/cyan italic]\n")
     else:
-        print("[red]You were too slow on the draw, \
-they took your food and clothes![/red]")
+        print("\n[red]You were too slow on the draw, \
+they took your food and clothes![/red]\n")
         vars.amount_spent_on_bullets -= (20 * response_time)
         vars.amount_spent_on_clothing -= (4 * response_time)
         vars.amount_spent_on_food -= (8 * response_time)
@@ -195,22 +201,22 @@ they took your food and clothes![/red]")
 
 def bandits_attack():
     """Random event for a bandit attack"""
-    print("[red]Bandits attack!")
+    print("\n[red]Bandits attack![/red]\n")
 # Determines shooting level, whether you miss or not, or have enough bullets
-    response_time = vars.shooting(vars.shooting_level)
+    response_time = vars.shooting()
     vars.amount_spent_on_bullets -= (20 * response_time)
 
     if vars.amount_spent_on_bullets < 0:
-        print("[red]You ran out of bullets! They take a bunch of\
- your [/red][green]money![/green]")
+        print("\n[red]You ran out of bullets! They take a bunch of\
+ your [/red][green]money![/green]\n")
         vars.cash_total /= 3
-        got_shot(vars)
+        vars.got_shot = True
     else:
         if response_time > 1:
-            got_shot(vars)
+            vars.got_shot = True
         else:
-            print("QUICKEST DRAW OUTSIDE OF DODGE CITY!!")
-            print("You got 'em!")
+            print("\nQUICKEST DRAW OUTSIDE OF DODGE CITY!!\n")
+            print("\nYou got 'em!\n")
 
 
 # events array
@@ -224,6 +230,3 @@ events_list = [weather, wagon_break_down, ox_injuries, arm_broke,
 def events():
     """Function for determining which event occurs"""
     random.choice(events_list)()
-
-
-events()
