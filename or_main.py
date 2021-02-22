520f592728e0ab7ff8ab31459e774ae7ba1263c3
JackMorash <60446354+JackMorash@users.noreply.github.com>
2021-02-22 11:05:33 -0600

    Update or_main.py

--- or_main.py
+++ or_main.py
@@ -1,68 +1,53 @@
-# Course: CS 30
-# Period: 1
-# Date Created: 21/02/04
-# Date last Modified: 21/02/05
-# Name: Jack Morash
-# Description: Final project for CS30,
-# a remake of the original Oregon Trail CLI game from 1978
-# originally written in BASIC, i've re-written it from scratch in Python.
-import time
-
 from rich import print
 from rich.console import Console
-from rich.markdown import Markdown
 
-import or_jobs
+import or_trail
 
-# Creates empty list for party member names
-mem_names = []
 console = Console()
-console.clear()
-
-console.clear()
 
 
-def main_menu():
-    """Function for displaying main menu"""
-    while True:
-        print("""
- _____                              _____         _ _ 
-|  _  |                            |_   _|       (_) |
-| | | |_ __ ___  __ _  ___  _ __     | |_ __ __ _ _| |
-| | | | '__/ _ \/ _` |/ _ \| '_ \    | | '__/ _` | | |
-\ \_/ / | |  __/ (_| | (_) | | | |   | | | | (_| | | |
- \___/|_|  \___|\__, |\___/|_| |_|   \_/_|  \__,_|_|_|
-                 __/ |                                
-                |___/
-        \n \n [u]Welcome to [bold red]Oregon Trail[/bold red]:\
-Python Edition![/u]\n""")
+def map():
+    """Array for locations in the game"""
+    console.clear()
+    map = [["[blue bold]OREGON \n CITY[/blue bold]", "   FORT WALLA WALLA"],
+           ["  ★", " "*10, "☐", "\n", " "*10, "◼"],
+           [" "*6, "THE DALLES", "\n", " "*14, "◼"],
+           [" "*14, "BLUE", " "*2, "FORT BOISE"],
+           [" "*12, "MOUNTAINS", " ", "☐"],
+           ["\n\n", " "*30, "FORT HALL"],
+           [" "*34, "☐",  " "*17, "INDEPENDENCE ROCK"],
+           [" "*40, "SODA SPRINGS", " "*3, "◼"],
+           [" "*40, "◼", " "*7, "◼"],
+           [" "*50, "SOUTH PASS", " "*4, "☐", " "*4, "CHIMNEY ROCK"],
+           [" "*63, "LARAMIE", " "*4, "◼"],
+           [" "*85, "☐\n", " "*76, "FORT KEARNEY"],
+           [" "*101, "[bold red]START[/bold red]"],
+           [" "*103, "★"],
+           [" "*98, "INDEPENDENCE"],
+           ]
 
-    # Menu option selection handlers
-        print("1.) Start")
-        print("2.) Exit")
-        print("3.) Changelog")
-        option = input("\n--> ")
-        if option == "start":
-            console.clear()
-            or_jobs.job()
-            break
-        elif option == "exit":
-            return False
-        elif option == "1":
-            console.clear()
-            or_jobs.job()
-            break
-        elif option == "2":
-            return False
-        elif option == "3":
-            console.clear()
-            with open("changelog.md") as md:
-                markdown = Markdown(md.read())
-            console.print(markdown)
-        else:
-            print("[bold red]Invalid Selection[/bold red]")
-            time.sleep(3)
-            continue
+# prints the map
+    spacer = " "*40
+    print(f"\n{spacer}[u italic cyan]Map of the Oregon Trail[/u italic cyan]")
+    for i in map:
+        for j in i:
+            print(j, end=" ")
+        print()
+    print("""
 
+* * * * * * * * * * * * *
+*                       *
+*      ☐ FORTS          *
+*      ◼ LANDMARKS      *
+*      _ YOUR ROUTE     *
+*      ★ HOME           *
+*                       *
+* * * * * * * * * * * * *
 
-main_menu()
+    """)
+    option = input("\n--> ")
+    if option == "exit":
+        return False
+    else:
+        console.clear()
+        or_trail.walking_trail()
